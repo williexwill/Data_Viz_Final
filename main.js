3240299e9ea786a9bfdfcd00947e64f834d30c29
Will Luckman <wmluckman@gmail.com>
2022-12-03 00:03:22 -0500

    chipping away

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ let sizeScale;
 /* APPLICATION STATE */
 let state = {
   data: [],
-//   selectedGraph: "All" // I can't figure out how to have my 
-//   dropdown accurately reflect the initial state
+  selectedGraph: "Total Workforce" 
 };
 console.log("state1", state);
 
@@ -68,29 +67,60 @@ colorScale = d3.scaleOrdinal()
 const yAxis = d3.axisLeft(yScale3)
     .tickFormat(d => (d * 100) + '%');
 
+// + TOOL TIPS
+const tooltip = d3.select("#container3")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "1px")
+  .style("border-radius", "5px")
+  .style("padding", "10px")
+
+const mouseover = function(event, d) {
+    tooltip
+        .style("opacity", 1)
+  }
+
+const mousemove = function(event, d) {
+    tooltip
+        .html(`${d['Selected Geographies']}`)
+        .style("display", "inline")
+        .style("position","fixed")
+        .style("left", (event.x)/2 + 'px') 
+        .style("top", (event.y)/2 + 'px')
+}
+
+const mouseleave = function(event,d) {
+  tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+}
 
   // + UI ELEMENT SETUP
-//   const selectElement = d3.select("#dropdown")
+const selectElement = d3.select("#dropdown")
 
-//   selectElement  
-//     .selectAll("option")
-//     .data([...Array.from(new Set(state.data.map(d => d.party))), "All"])
-//     .join("option")
-//     .attr("value", d => d)
-//     .attr("selected", d => (d === "All"))
-//     .text(d => d)
+selectElement  
+    .selectAll("option")
+    .data(["Total Workforce", "Unemployment", "Wages", "GDP"])
+    .join("option")
+    .attr("value", d => d)
+    .attr("selected", d => (d === "Total Workforce"))
+    .text(d => d)
 
-//   selectElement
-//     .on("change", (event) => {
-//       console.log(event)
-//       console.log("prev", state)
-//       state.selectedParty = event.target.value;
-//       console.log("post", state)
+selectElement
+    .on("change", (event) => {
+        console.log(event)
+        console.log("prev", state)
+        state.selectedGraph = event.target.value;
+        console.log("post", state)
 
-//       draw();
-//     })
+        draw();
+    })
 
-//   console.log(selectElement)
+  console.log(selectElement)
 
 
   // + CREATE SVG ELEMENT
@@ -100,7 +130,7 @@ svg = d3.select("#container3")
     .attr("height", height3)
     .style("background-color", "rgb(213 200 175)")
 
-  // + CALL AXES
+  // + CALL Y AXIS
 //   const xAxisGroup = svg.append("g")
 //     .attr("class", 'xAxis')
 //     .attr("transform", `translate(${0}, ${height3 - margin3.bottom})`) // move to the bottom
@@ -137,48 +167,48 @@ yAxisGroup.append("text")
 }
 
 // /* DRAW FUNCTION */
-// function draw() {
+function draw() {
 
 //   // + FILTER DATA BASED ON STATE
 // //   const filteredData = state.data
 // //     .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
 
-//   const dot = svg
-//     .selectAll("circle")
-//     .data(state.data, d => d["Selected Geographies"])
-//     .join(
-//       // + HANDLE ENTER SELECTION
-//       enter => enter.append("circle")
-//           .attr("r", 5)
-//           .attr("class", "dot")
-//           .attr("cx", d => xScale(d.ideology))
-//           .attr("cy", d => yScale(d.Score))
-//           .attr("fill", d => colorScale(d.Party))
-//           .call(sel => sel
-//             .transition()
-//             .duration(2000)
-//             .attr("r", d => sizeScale(d.Population))
-//           ),
+  const dot = svg
+    .selectAll("circle")
+    .data(state.data, d => d["Selected Geographies"])
+    .join(
+      // + HANDLE ENTER SELECTION
+      enter => enter.append("circle")
+          .attr("r", 5)
+          .attr("class", "dot")
+          //.attr("cx", d => xScale(d.ideology))
+          .attr("cy", d => yScale(d.Score))
+          .attr("fill", d => colorScale(d.Party))
+          .call(sel => sel
+            .transition()
+            .duration(2000)
+            .attr("r", d => sizeScale(d.Population))
+          ),
 
-//       // + HANDLE UPDATE SELECTION
-//       update => update
-//         .call(sel => sel.transition()
-//           .duration(1000)
-//           .attr("r", 5) 
-//           .transition()
-//           .duration(1000)
-//           .attr("r", d => sizeScale(d.Population))      
-//       ),
+      // + HANDLE UPDATE SELECTION
+      update => update
+        .call(sel => sel.transition()
+          .duration(1000)
+          .attr("r", 5) 
+          .transition()
+          .duration(1000)
+          .attr("r", d => sizeScale(d.Population))      
+      ),
 
-//       // + HANDLE EXIT SELECTION
-//       exit => exit
-//         .call(sel => sel
-//           .attr("opacity", 1)
-//           .transition()
-//           .duration(1000)
-//           .attr("opacity", 0)
-//           .attr("r", 5) 
-//           .remove()
-//           )
-//     );
-// }
+      // + HANDLE EXIT SELECTION
+      exit => exit
+        .call(sel => sel
+          .attr("opacity", 1)
+          .transition()
+          .duration(1000)
+          .attr("opacity", 0)
+          .attr("r", 5) 
+          .remove()
+          )
+    );
+}
